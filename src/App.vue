147b729536fcded2ff05001b176801691fb0aed2
Bheutschi <87085780+Bheutschi@users.noreply.github.com>
2026-01-15 14:16:01 +0100

    Change architecture and add the basic 3d scene (#19)
    * feat: update main.css to set width and height to 100% for full viewport usage
    
    * feat: replace vc-viewer with CesiumView component for improved structure
    
    * feat: replace vc-viewer with CesiumView component for improved structure
    
    * feat: add configuration constants for Cesium integration
    
    * feat: implement useCesiumCam hook for enhanced camera control and geolocation support
    
    * feat: add example environment file with Cesium access token placeholder
    
    * feat: add .env to .gitignore for environment file exclusion

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,9 @@
 <script setup>
-
-import VueCesium, {VcViewer} from "vue-cesium";
 import HelloWorld from "@/components/HelloWorld.vue";
-
-const onViewerReady = async (cesiumInstance) => {
-  const {viewer, Cesium} = cesiumInstance;
-  Cesium.Ion.defaultAccessToken = 'mettre votre clé ici';
-  try {
-    viewer.scene.terrainProvider = await Cesium.createWorldTerrainAsync();
-    viewer.scene.primitives.add(await Cesium.createOsmBuildingsAsync());
-  } catch (error) {
-    console.log("Erreur:", error);
-  }
-
-  viewer.scene.globe.enableLighting = true;
-
-  viewer.camera.setView({
-    destination: Cesium.Cartesian3.fromDegrees(6.6412, 46.7785, 437),
-    orientation: {
-      heading: Cesium.Math.toRadians(180),
-      pitch: Cesium.Math.toRadians(10),
-      roll: 0.0
-    }
-  });
-  viewer.camera.constrainedAxis = Cesium.Cartesian3.UNIT_Z;
-
-  const controller = viewer.scene.screenSpaceCameraController;
-
-  controller.enableZoom = false;
-  controller.enableTranslate = false;
-  controller.enableTilt = false;
-  controller.enableLook = true;
-  controller.rotateEventTypes = undefined;
-  controller.zoomEventTypes = undefined;
-  controller.tiltEventTypes = undefined;
-  controller.lookEventTypes = [
-    Cesium.CameraEventType.LEFT_DRAG,
-    Cesium.CameraEventType.PINCH
-  ];
-
-}
+import CesiumView from "@/components/CesiumView.vue";
 </script>
 
 <template>
   <hello-world msg="Hello Vue 3!"></hello-world>
-  <vc-viewer
-      @ready="onViewerReady"
-      :animation="false"
-      :timeline="false"
-      :base-layer-picker="false"
-  >
-  </vc-viewer>
+  <CesiumView/>
 </template>
---
+++ src/components/CesiumView.vue
@@ -0,0 +1,29 @@
+<script setup>
+import {VcImageryProviderArcgis, VcLayerImagery, VcViewer} from "vue-cesium";
+import {useCesiumCam} from "@/composables/useCesiumCam.js";
+
+const {setupScene} = useCesiumCam();
+
+const onViewerReady = (cesiumInstance) => {
+  setupScene(cesiumInstance);
+};
+</script>
+
+<template>
+  <div class="viewer-container">
+    <vc-viewer
+        :animation="false"
+        :base-layer-picker="false"
+        :timeline="false"
+        @ready="onViewerReady"
+    >
+      <vc-layer-imagery>
+        <vc-imagery-provider-arcgis/>
+      </vc-layer-imagery>
+    </vc-viewer>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
